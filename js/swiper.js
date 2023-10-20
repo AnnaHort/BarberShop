e6321f35ea69da3a0df661fe353f0f7e4578f141
AnnaHort <annahort3d@gmail.com>
2023-10-21 00:39:42 +0300

    testimonials-section mobile

--- js/swiper.js
+++ js/swiper.js
@@ -5,32 +5,33 @@ new Swiper(".swiper", {
   },
   loop: true,
   // автопрокрутка
-  autoplay: {
-    // // пауза між прокруткою
-    delay: 3000,
-    //     // закінчити на останньому слайді
-    stopOnLastSlide: true,
-    //     // відключити після ручного переключення
-    disableOnInteraction: false,
-  },
-  // швидікість автопрокруту
-  speed: 800,
+  // autoplay: {
+  //   // // пауза між прокруткою
+  //   delay: 3000,
+  //   //     // закінчити на останньому слайді
+  //   stopOnLastSlide: true,
+  //   //     // відключити після ручного переключення
+  //   disableOnInteraction: false,
+  // },
+  // // швидікість автопрокруту
+  // speed: 1000,
 
-//   БРЕКПОІНТ / АДАПТИВ /
-//   ширина екрану
-//   (виставити значення slidesPerView: 'auto')
-    breakpoints: {
-      375: {
-        slidesPreView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-       1024: {
-        slidesPerView: 3,
-      },
-      1200: {
-        slidesPerView: 4,
-      },
+  //   БРЕКПОІНТ / АДАПТИВ /
+  //   ширина екрану
+  //   (виставити значення slidesPerView: 'auto')
+  breakpoints: {
+    375: {
+      slidesPreView: 1,
+      spaceBetween: 20,
     },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
 });
