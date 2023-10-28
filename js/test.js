08e4d9dc81eaef05fa019e051d71e24ef28062f2
AnnaHort <annahort3d@gmail.com>
2023-10-28 18:10:01 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,1 +1,13 @@
+const findShort = (s) => {
 
+  let arr = []
+
+  const words = s.split(" ");
+  
+  words.forEach((word) => {
+    const wordLength = word.length;
+    arr.push(wordLength);
+  });
+  return(console.log(Math.min(...arr)));
+};
+findShort("Hello Anna! How are you?");
