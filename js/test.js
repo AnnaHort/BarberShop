887073b141693f2c3a89d4e265702cbc4e4b0708
AnnaHort <annahort3d@gmail.com>
2023-11-05 12:37:34 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,10 +1,9 @@
-const XO = (str) => {
-  if (str.match(/o/gi) === null && str.match(/x/gi) === null) {
-    return true;
-  }
-  return str.match(/o/gi).length === str.match(/x/gi).length;
-};
+// чи являється строка яка містить тільки букви - ізограмою (слово в якому немає букв які повторюються) (пуста строка = ізограма)
 
+const isIsogram = (str) => {
+console.log(str);
 
+}
 
-XO("oOxx");
+isIsogram("Dermatoglyphics");
+// isIsogram(moose);
