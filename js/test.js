126fa9a7ea0eda5b5689b0f5761833ed97b7197b
AnnaHort <annahort3d@gmail.com>
2023-11-14 23:15:55 +0200

    js

--- js/test.js
+++ js/test.js
@@ -1,16 +1,1 @@
-// const toUpperCase = string => {
-//   const words = string.split(" ");
-//   const capitalizedWords = words.map((word) => {
-//     const firstLetter = word[0].toUpperCase();
-//     return firstLetter + word.slice(1);
-//   });
-//   return capitalizedWords.join(" ");
-// };
-
-const toJadenCase = (string) => {
-    return string.toJadenCase();
-  }
-
-toJadenCase("hello anna! How are you?");
-
 
