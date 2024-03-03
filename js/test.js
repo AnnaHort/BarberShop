8bcd7224f7cb84750264398d35cbec15d19813ac
AnnaHort <annahort3d@gmail.com>
2024-03-03 16:37:31 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,1 +1,31 @@
+function dotCalculator(arr) {
+  let casa = 0;
 
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] === 25) {
+      casa += 25;
+    }
+    if (arr[i] > 25) {
+      if (casa - arr[i] < 0) {
+        console.log("false", false);
+        return
+      } else {
+        casa += arr[i];
+        casa -= arr[i] - 25;
+      }
+    }
+    if (casa < 0) {
+      console.log("false", false);
+      // return false;
+    }
+    console.log("true", true);
+    console.log("casa", casa);
+    // return true;
+  }
+}
+
+// dotCalculator([25, 25, 50, 25, 75, 25, 25, 100]);
+dotCalculator([25, 25, 50, 25, 100, 100, 175]);
+
+// [25, 25, 50, 25, 75, 25, 25, 100] true;
+// [25, 25, 50, 75, 50] false
