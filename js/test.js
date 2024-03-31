d23b21b42b4ee1c5e0d977ed15e9f00044b878d3
AnnaHort <annahort3d@gmail.com>
2024-03-31 18:23:10 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,31 +1,18 @@
-function dotCalculator(arr) {
-  let casa = 0;
+function replace(s) {
+  const str = s.split("");
 
-  for (let i = 0; i <= arr.length; i++) {
-    if (arr[i] === 25) {
-      casa += 25;
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      str[i] = "!";
     }
-    if (arr[i] > 25) {
-      if (casa - arr[i] < 0) {
-        console.log("false", false);
-        return
-      } else {
-        casa += arr[i];
-        casa -= arr[i] - 25;
-      }
-    }
-    if (casa < 0) {
-      console.log("false", false);
-      // return false;
-    }
-    console.log("true", true);
-    console.log("casa", casa);
-    // return true;
   }
+  return str.join("");
 }
 
-// dotCalculator([25, 25, 50, 25, 75, 25, 25, 100]);
-dotCalculator([25, 25, 50, 25, 100, 100, 175]);
-
-// [25, 25, 50, 25, 75, 25, 25, 100] true;
-// [25, 25, 50, 75, 50] false
+replace("sssssoss");
+// replace("Hi!") === "H!!";
+// replace("!Hi! Hi!") === "!H!! H!!";
+// replace("aeiou") === "!!!!!";
+// replace("ABCDE") === "!BCD!";
