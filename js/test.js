89423b0c866f74254e24f5369e09f4c39010c5dd
AnnaHort <annahort3d@gmail.com>
2024-06-26 01:13:29 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,13 +1,10 @@
-function noSpace(x) {
-  console.log(x);
-  let y = 0;
-  for (let i = 0; i < x.length; i++) {
-    console.log(x[i]);
-    if (x[i].length > 0) {
-      y += x[i];
-    }
+function solution(a, b) {
+  console.log(a.length);
+  if (a.length > b.length) {
+    console.log(b + a + b);
+  } else {
+    a + b + a;
   }
-  console.log(y);
 }
 
-noSpace("8 j 8   mBliB8g  imjB8B8  jl  B");
+solution("111", "22");
